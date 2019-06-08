3cca4bca2ac74155a31467eaa4e85f94e93d7ee8
arun <arunreddy.iosdev@gmail.com>
2019-06-08 11:07:06 +0530

    Initial Commit

---
+++ coredata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coredata
+//
+//  Created by Arun Reddy on 08/06/19.
+//  Copyright © 2019 ITS ME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
